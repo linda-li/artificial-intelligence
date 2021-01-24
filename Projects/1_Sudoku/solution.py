8a817b19ae24a5b9d03245a9b59e472ade866623
Linda Li <lindali.va@gmail.com>
2021-01-24 11:09:19 -0500

    Add progress on project 1 - Sudoku Solver

--- Projects/1_Sudoku/solution.py
+++ Projects/1_Sudoku/solution.py
@@ -1,6 +1,6 @@
 
 from utils import *
-
+import copy
 
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
@@ -8,8 +8,9 @@ square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','45
 unitlist = row_units + column_units + square_units
 
 # TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
-
+diagonal_units = [[r+c for r, c in zip(rows, cols)]]
+rev_diagonal_units = [[r+c for r, c in zip(rows, reversed(cols))]]
+unitlist = unitlist + diagonal_units + rev_diagonal_units
 
 # Must be called after all units (including diagonals) are added to the unitlist
 units = extract_units(unitlist, boxes)
@@ -54,7 +55,15 @@ def naked_twins(values):
     https://github.com/udacity/artificial-intelligence/blob/master/Projects/1_Sudoku/pseudocode.md
     """
     # TODO: Implement this function!
-    raise NotImplementedError
+    tempVals = copy.deepcopy(values)
+
+    for boxA in values.keys():
+        for boxB in peers[boxA]:
+            if values[boxA] == values[boxB] and (len(values[boxA])==2) and (len(values[boxB])==2):
+                for peer in peers[boxA].intersection(peers[boxB]):
+                    for digit in values[boxA]:
+                        tempVals[peer] = tempVals[peer].replace(digit,'')
+    return tempVals
 
 
 def eliminate(values):
@@ -74,7 +83,15 @@ def eliminate(values):
         The values dictionary with the assigned values eliminated from peers
     """
     # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    solvedBoxes = [b for b in values.keys() if len(values[b]) == 1]
+    
+    for k in solvedBoxes:
+        num = values[k]
+        for peer in peers[k]:
+            if len(values[peer]) > 1:
+                values[peer] = values[peer].replace(num, '')
+
+    return values
 
 
 def only_choice(values):
@@ -98,7 +115,12 @@ def only_choice(values):
     You should be able to complete this function by copying your code from the classroom
     """
     # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    for unit in unitlist:
+        for num in '123456789':
+            numPlaces = [box for box in unit if num in values[box]]
+            if len(numPlaces) == 1:
+                values[numPlaces[0]] = num
+    return values
 
 
 def reduce_puzzle(values):
@@ -116,8 +138,24 @@ def reduce_puzzle(values):
         no longer produces any changes, or False if the puzzle is unsolvable 
     """
     # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
-
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        # Your code here: Use the Eliminate Strategy
+        values = eliminate(values)
+        # Your code here: Use the Only Choice Strategy
+        values = only_choice(values)
+        # Naked Twins
+        values = naked_twins(values)
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
     """Apply depth first search to solve Sudoku puzzles in order to solve puzzles
@@ -139,8 +177,21 @@ def search(values):
     and extending it to call the naked twins strategy.
     """
     # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
-
+    values = reduce_puzzle(values)
+    if values is False:
+        return False
+    if all(len(values[s]) == 1 for s in boxes):
+        return values
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    
+    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """Find the solution to a Sudoku puzzle using search and constraint propagation
